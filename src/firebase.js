f4b272cfad4ff18b47a46e2d1e26e2bc4e6972e3
omari <omari.jalagania@gmail.com>
2021-10-19 00:09:53 +0400

    variables

--- src/firebase.js
+++ src/firebase.js
@@ -2,12 +2,12 @@ import firebase from "firebase/compat/app";
 import "firebase/compat/auth";
 
 const firebaseConfig = firebase.initializeApp({
-  apiKey: "AIzaSyC74EbnRpDhZ68vijD_a9RRW4wcGSaPR2A",
-  authDomain: "todoauth-9329f.firebaseapp.com",
-  projectId: "todoauth-9329f",
-  storageBucket: "todoauth-9329f.appspot.com",
-  messagingSenderId: "280970739609",
-  appId: "1:280970739609:web:5e1a580c4e9a170f75dd08",
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE,
+  messagingSenderId: process.env.REACT_APP_MESSAGE_SENDER,
+  appId: process.env.REACT_APP_APP_ID,
 });
 
 export default firebaseConfig;
